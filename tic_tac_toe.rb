d202f6e362efa7404dea5675acf939993d9b3ab1
Hugo <huwgoma@gmail.com>
2021-06-16 19:04:30 -0400

    Building game start prompting functionality - Get Player 1/Player 2's names, set current player to PLayer 1, and prompt the current player to enter a number. Initialize and display the tic-tac-toe board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,58 +1,89 @@
+require 'pry'
 class Board
-
-  private
-  attr_reader :cells
-  def initialize(number)
+  def initialize
     @cells = Array(1..9)
   end
 
+  attr_reader :cells
+  
   def display_board
     puts <<-BOARD
-      #{cells[0]} | #{cells[1]} | #{cells[2]}
+       #{cells[0]} | #{cells[1]} | #{cells[2]}
       ---+---+---
-      #{cells[3]} | #{cells[4]} | #{cells[5]}
+       #{cells[3]} | #{cells[4]} | #{cells[5]}
       ---+---+---
-      #{cells[6]} | #{cells[7]} | #{cells[8]}
+       #{cells[6]} | #{cells[7]} | #{cells[8]}
     BOARD
   end
+
+  private
+
 end
 
 
 class Player 
   @@number_of_players = 0
 
-  private
   def initialize(name)
     @name = name
     @@number_of_players += 1
     @player_id = @@number_of_players
     @player_symbol = (@player_id == 1)? "X" : "O"  
   end
+
+  attr_reader :name, :player_id, :player_symbol
+
+  private
 end
 
 
 class Game
+
+  attr_reader :player_1, :player_2
+  attr_accessor :current_player
+
+  def initialize
+    self.instantiate_players(get_player_1_name, get_player_2_name)
+    @current_player = player_1
+    @board = Board.new
+    @board.display_board
+    self.prompt_current_player_move
+    
+  end
   
-  def prompt_player_1
+  private 
+
+  def get_player_1_name
     puts "What is Player 1's name?"
     @player_1_name = gets.chomp.to_s
   end
   
-  def prompt_player_2
+  def get_player_2_name
     puts "What is Player 2's name?"
     @player_2_name = gets.chomp.to_s
   end
   
-  def instantiate_players
-    Player.new(@player_1_name)
-    Player.new(@player_2_name)
+  def instantiate_players(player_1_name, player_2_name)
+    @player_1 = Player.new(player_1_name)
+    @player_2 = Player.new(player_2_name)
   end
+
+  def prompt_current_player_move
+    puts "#{current_player.name}, enter a number (1-9) corresponding to the grid cell you want to mark."
+  end
+
+
+  
+  
 end
 
-game = Game.new
-game.prompt_player_1
-game.prompt_player_2
-game.instantiate_players
+Game.new
+
+a = "a"
+
+# game.prompt_player_1
+# game.prompt_player_2
+# game.instantiate_players
 
 
 
