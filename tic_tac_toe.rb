e71905ab0fa4606c01b75a37e34c13c98fb8d21e
Hugo <huwgoma@gmail.com>
2021-06-16 21:10:46 -0400

    Prompt the current player(Player 1) to enter a number; check if the number is valid, if it is, update the Board cells array and re-display the board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -39,16 +39,28 @@ end
 
 class Game
 
-  attr_reader :player_1, :player_2
+  attr_reader :player_1, :player_2, :board, :current_player_move
   attr_accessor :current_player
 
   def initialize
     self.instantiate_players(get_player_1_name, get_player_2_name)
     @current_player = player_1
-    @board = Board.new
-    @board.display_board
-    self.prompt_current_player_move
     
+    self.instantiate_board
+    
+    
+  end
+
+  def play_game
+    board.display_board
+    
+    self.get_current_player_move
+
+    self.reprompt_input
+    self.update_cells
+    board.display_board
+    
+    binding.pry
   end
   
   private 
@@ -68,16 +80,37 @@ class Game
     @player_2 = Player.new(player_2_name)
   end
 
-  def prompt_current_player_move
-    puts "#{current_player.name}, enter a number (1-9) corresponding to the grid cell you want to mark."
+  def instantiate_board
+    @board = Board.new
   end
 
+  def get_current_player_move
+    puts "#{current_player.name}, enter a number (1-9) corresponding to the grid cell you want to mark."
+    @current_player_move = gets.to_i
+  end
+
+  def valid_number?
+    board.cells.include?(current_player_move)
+  end
+
+  def reprompt_input
+    while(!valid_number?)
+      puts "Please enter a valid number!"
+      self.get_current_player_move
+    end
+  end
+
+  def update_cells
+    target_index = board.cells.find_index(current_player_move)
+    board.cells[target_index] = current_player.player_symbol
+  end
+  
 
   
   
 end
 
-Game.new
+Game.new.play_game
 
 a = "a"
 
